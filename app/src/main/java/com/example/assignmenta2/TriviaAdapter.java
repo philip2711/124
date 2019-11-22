4653b66562cd441ca97a77558f837168029fd9ab
Philip Alava <z5205952@ad.unsw.edu.au>
2019-11-22 13:05:40 +1100

    Initial commit

---
+++ app/src/main/java/com/example/assignmenta2/TriviaAdapter.java
@@ -0,0 +1,121 @@
+package com.example.assignmenta2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class TriviaAdapter extends RecyclerView.Adapter<TriviaAdapter.TriviaViewHolder> {
+    public static List<Trivia> triviaToAdapt;
+    public static int score = 0;
+
+    public void setData(List<Trivia> triviaToAdapt) {
+        this.triviaToAdapt = triviaToAdapt;
+    }
+
+    @NonNull
+    @Override
+    public TriviaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view =
+                LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.trivia, parent, false);
+
+        TriviaViewHolder triviaViewHolder = new TriviaViewHolder(view);
+        return triviaViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TriviaViewHolder holder, int position) {
+        final Trivia triviaAtPosition = triviaToAdapt.get(position);
+        final Context context = holder.view.getContext();
+        holder.bind(triviaAtPosition);
+    }
+
+    @Override
+    public int getItemCount() {
+        return triviaToAdapt.size();
+    }
+
+    public static class TriviaViewHolder extends RecyclerView.ViewHolder {
+        public View view;
+        public TextView questionText;
+        public TextView answert;
+        public Button answerb;
+        public Button wrongt;
+        public TextView wrongp;
+        public TextView value;
+        public TextView heading;
+
+        public TriviaViewHolder(View v){
+            super(v);
+            view = v;
+            questionText = v.findViewById(R.id.questionText);
+            answert = v.findViewById(R.id.answertext);
+            answerb = v.findViewById(R.id.answerbutton);
+            wrongt = v.findViewById(R.id.wrong);
+            wrongp = v.findViewById(R.id.wrongprompt);
+            value = v.findViewById(R.id.valuetext);
+            heading = v.findViewById(R.id.questionn);
+        }
+        public void bind(final Trivia trivia) {
+
+            questionText.setText(trivia.getQuestion());
+            value.setText("Value: " + Integer.toString(trivia.getValue()));
+
+            answerb.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String s1 = trivia.getAnswer();
+                    String replaceString = s1.replace("<i>","");
+                    String s2 = replaceString;
+                    final String replaceString3 = s2.replace("</i>", "");
+                    answert.setText(replaceString3);
+                    answert.setTextColor(Color.GREEN);
+                    score += trivia.getValue();
+                    Toast.makeText(v.getContext(), String.valueOf(score), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            wrongt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String s1 = trivia.getAnswer();
+                    String replaceString = s1.replace("<i>","");
+                    String s2 = replaceString;
+                    final String replaceString3 = s2.replace("</i>", "");
+                    answert.setText(replaceString3);
+                    answert.setTextColor(Color.RED);
+                    wrongp.setText("Still don't understand the right answer? Click here to find out more!");
+                    wrongp.setVisibility(View.VISIBLE);
+                    wrongp.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent implicit = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=" + trivia.getQuestion()));
+                            v.getContext() .startActivity(implicit);
+                        }
+                    });
+                }
+            });
+
+        }
+    }
+}
